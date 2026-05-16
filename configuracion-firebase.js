df6624fb3ad695bb97ab3d617cffafdb8572a70e
patronatozacualpan <manuelins891@gmail.com>
2026-05-15 21:21:01 -0700

    Update configuracion-firebase.js

--- configuracion-firebase.js
+++ configuracion-firebase.js
@@ -39,7 +39,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAKoDtHyAwjVd02M-MjcuQ0awoIDxhvJKc",
   authDomain: "donativos-patronato-zacualpan.firebaseapp.com",
   projectId: "donativos-patronato-zacualpan",
-  storageBucket: "donativos-patronato-zacualpan.firebasestorage.app",
+  storageBucket: "donativos-patronato-zacualpan.appspot.com",
   messagingSenderId: "173840953766",
   appId: "1:173840953766:web:8a9bfa2ec7328fb1989f67"
 };
