733b8222f90c324123e7b0e10445c2a8e064ddb1
patronatozacualpan <manuelins891@gmail.com>
2026-06-25 22:43:02 -0700

    Update index.js

--- js/modules/benefactores/index.js
+++ js/modules/benefactores/index.js
@@ -1,11 +1,45 @@
 /*
-====================================================
+======================================================
 
-MÓDULO
-
-MURO DE BENEFACTORES
+MÓDULO BENEFECTORES
 
 Patronato Zacualpan
+Protección Civil Zacualpan
 
-====================================================
+Archivo principal del módulo.
+
+Su única responsabilidad es iniciar
+los componentes del módulo.
+
+======================================================
 */
+
+window.PCZ = window.PCZ || {};
+
+window.PCZ.Benefactores = {
+
+    iniciar(){
+
+        console.log(
+            "✓ Módulo Benefactores iniciado."
+        );
+
+    }
+
+};
+
+/*=========================================
+INICIAR
+=========================================*/
+
+document.addEventListener(
+
+    "DOMContentLoaded",
+
+    ()=>{
+
+        window.PCZ.Benefactores.iniciar();
+
+    }
+
+);
